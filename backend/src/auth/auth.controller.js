6ae804dff5a09218508241b4f55af59aa10ffcb3
Prakhar Sachan <sprakhar639@gmail.com>
2026-09-25 03:41:00 +0530

    Implemented login feature

--- backend/src/auth/auth.controller.js
+++ backend/src/auth/auth.controller.js
@@ -1,10 +1,11 @@
 import { register, login, logout } from "./auth.service.js";
 async function userRegister(req, res) {
   const user = await register(req.body);
-  res.status(200).json({ message: "Registed Successfully"},user);
+  res.status(200).json({ message: "Registed Successfully",user});
 }
 async function userLogin(req, res) {
-
+ const user=await login(req.body);
+ res.status(200).json({ message: "LoggedIn Successfully",user})
 }
 
 async function userLogout() {}
--- backend/src/auth/auth.service.js
+++ backend/src/auth/auth.service.js
@@ -1,15 +1,15 @@
-import bcrypt from "bcrypt";
 import bcrpyt from "bcrypt";
 import db from "../prisma/db.ts";
+import { or } from "@prisma/orm-postgres/orm-client";
 
 async function register({ username, email, password, name }) {
   try {
     const passwordHash = await bcrpyt.hash(password, 10);
     const user = await db.orm.public.User.create({
-        email,
-        username,
-        name,
-        passwordHash
+      email,
+      username,
+      name,
+      passwordHash,
     });
     return user;
   } catch (error) {
@@ -17,7 +17,12 @@ async function register({ username, email, password, name }) {
   }
 }
 
-async function login({ username, email, password }) {}
+async function login({ identifier, password }) {
+  const user = await db.orm.public.User.where((u) =>
+    or(u.email.eq(identifier), u.username.eq(identifier)),
+  ).first();
+  return user;
+}
 
 async function logout() {}
 export { register, login, logout };
